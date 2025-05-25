e25544d80883265fae09e9f63927185bf68f27cf
Valtin83 <106901202+Valtin83@users.noreply.github.com>
2025-05-25 23:54:25 +0700

    TelegramBot start.

--- SpringDemoBot/src/main/java/com/example/SpringDemoBot/config/BotConfig.java
+++ SpringDemoBot/src/main/java/com/example/SpringDemoBot/config/BotConfig.java
@@ -1,9 +1,7 @@
 package com.example.SpringDemoBot.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ConfigurationProperties(prefix = "bot")
