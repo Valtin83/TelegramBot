2d2351a207a492f6389e20112fbfd5546c46a7ab
Valtin83 <106901202+Valtin83@users.noreply.github.com>
2025-07-22 20:26:14 +0700

    simple web applications

---
+++ simple web applications/simple-web-applications/src/main/java/com/example/simple/web/applications/SimpleWebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simple.web.applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebApplicationsApplication.class, args);
+	}
+
+}
